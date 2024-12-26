1d1b385b9176a02686b79e9b1187e0791be198da
Sayantan Nandi <officialsayantannnandi@gmail.com>
2024-12-26 23:15:12 +0530

    Temporary commit

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const path = require('path');
 const app = express();
 const passport = require('passport');
 const userModel = require('./models/users');
+const inventoryModel = require('./models/inventory');
+const reservationModel = require('./models/reservations');
+const orderModel = require('./models/orders');
 const localStrategy = require('passport-local');
 const session = require('session');
 const expressSession = require('express-session');
+const { name } = require('ejs');
 passport.use(new localStrategy(userModel.authenticate()));
 
 app.set('view engine', 'ejs');
@@ -53,12 +57,65 @@ app.get('/signup',(req,res)=>{
     res.render("signup");
 });
 
-app.get('/home', isLoggedIn, (req,res)=>{
-    res.render("home");
+app.get('/home', isLoggedIn, async (req,res)=>{
+    const inventory = await inventoryModel.findOne({
+        _id:'676aaaf304cb940ba556cee3'
+    });
+    const foodData = inventory.foodItems;
+    const user = await userModel.findOne({
+        username: req.session.passport.user,
+    });
+    let name = '';
+    for (let i = 0; i < user.fullName.length; i++){
+        if (user.fullName[i] == ' '){
+            break;
+        }
+        name += user.fullName[i];
+    }
+    res.render("home",{user:user,name:name,foodData:foodData});
 });
 
-app.get('/profile', isLoggedIn, (req,res)=>{
-    res.render("profile");
+app.get('/api/food', async (req, res) => {
+    const inventory = await inventoryModel.findOne({
+        _id:'676aaaf304cb940ba556cee3'
+    });
+    const foodData = inventory.foodItems;
+    res.json(foodData);
+});
+
+app.get('/api/reservations', async (req, res) => {
+    const currentUser = await userModel.findOne({
+        username: req.session.passport.user,
+    });
+    await currentUser.populate('reservations');
+    const reservationData = currentUser.reservations;
+    res.json(reservationData);
+});
+
+app.post("/order",async (req,res)=>{
+    const user = await userModel.findOne({
+        username: req.session.passport.user,
+    });
+    let reservationData = new reservationModel({
+        user: user._id,
+        name: req.body.food,
+        price: req.body.price,
+        quantity: req.body.quantity,
+        duration: req.body.duration,
+    });
+    await reservationData.save();
+    await reservationData.populate('user');
+    user.reservations.push(reservationData._id);
+    await user.save();
+    res.redirect('/profile');
+});
+
+app.get('/profile', isLoggedIn, async (req,res)=>{
+    const user = await userModel.findOne({
+        username: req.session.passport.user,
+    });
+    await user.populate('reservations');
+    res.render("profile",{user});
 });
 
 app.get('/logout',(req,res,next)=>{
